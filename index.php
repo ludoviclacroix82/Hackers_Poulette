1585ccfd0e35d7fa5ff43ee0de8f1b6be5675f2e
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-06-10 15:54:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+
+$arrayGender = array(
+    '' => 'Select your gender',
+    'male' => 'Male',
+    'female' => 'Female',
+    'nonbinary' => 'Non-binary',
+    'noreply' => 'Prefer not to say'
+);
+
+$arraySubject = array(
+    '' => 'Select a subject',
+    'order_issue' => 'Order Issue',
+    'technical_support' => 'Technical Support',
+    'general_inquiry' => 'General Inquiry'
+);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackers Poulette</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="form-control from">
+            <?php 
+
+                if(!empty($_SESSION['sendMail'])){
+                    if($_SESSION['sendMail'] == true){    
+                        echo '<div class="alert alert-success" role="alert">Your email has been successfully sent, and a copy has been sent to you at the address <b>'.$_SESSION['email'].'</b></div>';
+                        session_destroy();
+                    }else{
+                        echo '<div class="alert alert-danger" role="alert">YMessage could not be sent.</div>';
+
+                    }
+
+                }
+            ?>
+            
+            <form method="post" action="process_form.php">
+                <div class="form-group">
+                    <label for="object">Select a subject:</label>
+                    <select class="form-control" name="gender" id="gender" aria-label="Select your gender">
+                        <?php
+                        foreach ($arrayGender as $key => $value) {
+                            $disabled = ($key == '') ? 'disabled' : '';
+                            $selected = (!empty($_SESSION['gender']) && $_SESSION['gender'] == $key) ? 'selected' : '';
+                            echo '<option value="' . $key . '" ' . $selected . ' ' . $disabled . '>' . $value . '</option>';
+                        }
+                        ?>
+                    </select>
+                    <div class="error">
+                        <?php echo (!empty($_SESSION['errorGender'])) ? $_SESSION['errorGender'] : ''; ?>
+                    </div>
+                </div>
+                <div class="form-group ">
+                    <div class="userName">
+                        <input type="text" class="form-control" name="name" id="name" aria-label="Your name" placeholder="Name" value="<?php echo (!empty($_SESSION['name'])) ? $_SESSION['name'] : ''; ?>">
+                        <input type="text" class="form-control" name="lastName" id="lastName" aria-label="Your last name" placeholder="Last name" value="<?php echo (!empty($_SESSION['lastName'])) ? $_SESSION['lastName'] : ''; ?>">
+                    </div>
+                    <div class="error">
+                        <?php echo (!empty($_SESSION['errorName']) || !empty($_SESSION['errorLastName'])) ? $_SESSION['errorName'] . '<br>' . $_SESSION['errorLastName'] : ''; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" name="email" id="email" aria-label="Your Email" placeholder="Email" value="<?php echo (!empty($_SESSION['email'])) ? $_SESSION['email'] : ''; ?>">
+                    <div class="error"><?php echo (!empty($_SESSION['errorEmail'])) ? $_SESSION['errorEmail'] : ''; ?></div>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="country" id="country" aria-label="Your country" placeholder="Country" value="<?php echo (!empty($_SESSION['country'])) ? $_SESSION['country'] : ''; ?>">
+                    <div class="error">
+                        <?php echo (!empty($_SESSION['errorCountry'])) ? $_SESSION['errorCountry'] : ''; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="subject">Select a subject:</label>
+                    <select class="form-control" name="subject" id="subject" aria-label=">Select a subject">
+                        <?php
+                        foreach ($arraySubject as $key => $value) {
+                            $disabled = ($key == '') ? 'disabled' : '';
+                            $selected = ($_SESSION['subject'] == $key) ? 'selected' : '';
+                            echo '<option value="' . $key . '" ' . $selected . ' ' . $disabled . '>' . $value . '</option>';
+                        }
+                        ?>
+                    </select>
+                    <div class="error">
+                        <?php echo (!empty($_SESSION['errorSubject'])) ? $_SESSION['errorSubject'] : ''; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" id="message" name="message" rows="6" aria-label="Your message" placeholder="Your message"><?php echo (!empty($_SESSION['message'])) ? $_SESSION['message'] : ''; ?></textarea>
+                    <div class="error">
+                        <?php echo (!empty($_SESSION['errorMessage'])) ? $_SESSION['errorMessage'] : ''; ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
---
+++ process_form.php
@@ -0,0 +1,105 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require './assets/vendor/PHPMailer/src/Exception.php';
+require './assets/vendor/PHPMailer/src/PHPMailer.php';
+require './assets/vendor/PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    session_start();
+    $error = 0;
+
+    //récuperation des data Post
+    $gender = htmlspecialchars(trim($_POST['gender']));
+    $genderSend = ($gender === 'noreply')?'':$gende; // Si le user n'a pas voulu mettre son genre 
+
+    $name = htmlspecialchars(trim($_POST['name']));
+    $lastName = htmlspecialchars(trim($_POST['lastName']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $country = htmlspecialchars(trim($_POST['country']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    $_SESSION['gender'] = $gender;
+    $_SESSION['name'] = $name;
+    $_SESSION['lastName'] = $lastName;
+    $_SESSION['email'] = $email;
+    $_SESSION['country'] = $country;
+    $_SESSION['subject'] = $subject;
+    $_SESSION['message'] = $message;
+
+
+    $error += checkEmail($email);
+    $error += issetInput($gender, 'gender', 'errorGender', 'Please note that the Gender field is mandatory.');
+    $error += issetInput($name, 'name', 'errorName', 'Please note that the Name field is mandatory.');
+    $error += issetInput($lastName, 'lastName', 'errorLastName', 'Please note that the last name field is mandatory.');
+    $error += issetInput($country, 'country', 'errorCountry', 'Please note that the Country field is mandatory.');
+    $error += issetInput($message, 'message', 'errorMessage', 'Please note that the Message field is mandatory.');
+    $error += issetInput($subject, 'subject', 'errorSubject', 'Please note that the Subject field is mandatory.');
+
+
+
+    if ($error === 0) {
+        // Initialisation de l'objet PHPMailer
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();
+            $mail->Host = 'smtp.office365.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'lac_ludo@hotmail.com';
+            $mail->Password = '';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+
+            // Destinataires
+            $mail->setFrom($email, $name);
+            $mail->addAddress('lac_ludo@hotmail.com', 'Destinataire');
+
+            // Contenu de l'email
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $genderSend.''.$name . ' ' . $lastName . ' from:' . $country . '<br>' . $message;
+
+            $mail->send();
+            $_SESSION['sendMail'] = true;
+            header("Location: index.php");
+        } catch (Exception $e) {
+            //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            $_SESSION['sendMail'] = false;
+            header("Location: index.php");
+        }
+    } else {
+        header("Location: index.php");
+    }
+} else {
+    header("Location: index.php");
+    exit();
+}
+
+function checkEmail(string $email)
+{
+    //verification d'email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['errorEmail'] = 'The email is not valid.';
+        return 1;
+    } else {
+        $_SESSION['errorEmail'] = '';
+    }
+}
+
+function issetInput(string $input, string $session, string $errorSession, string $errorMessage)
+{
+    if (empty($input)) {
+        $_SESSION[$errorSession] = $errorMessage;
+        return 1;
+    } else {
+        $_SESSION[$errorSession] = '';
+    }
+}
+?>
